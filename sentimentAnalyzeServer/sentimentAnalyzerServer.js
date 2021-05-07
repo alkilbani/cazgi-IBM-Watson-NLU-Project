b27a7632471d6fd523f5851aaedcfd89e1c1f2ef
alkilbani <alkilbani@gmail.com>
2021-05-07 07:32:52 +0000

    NLU added to server

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,4 +1,25 @@
 const express = require('express');
+const dotnev = require('dotnev');
+dotnev.config();
+
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const NaturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return NaturalLanguageUnderstanding;
+}
+
+
 const app = new express();
 
 app.use(express.static('client'))
